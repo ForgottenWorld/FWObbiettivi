cdbb94fdf393c80be98ba321192dbc1c88bccc6a
Marco Demicheli <marco.demicheli.27@gmail.com>
2020-05-27 23:42:12 +0200

    Init

---
+++ src/main/java/it/forgottenworld/forgottenobbiettivi/ForgottenObbiettivi.java
@@ -0,0 +1,17 @@
+package it.forgottenworld.forgottenobbiettivi;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ForgottenObbiettivi extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
