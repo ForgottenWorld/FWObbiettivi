bc155275db0d7bcf178c000b8e70f18ddbcf5fdb
Markus27 <marco.demicheli.27@gmail.com>
2020-10-06 14:20:28 +0200

    Started working on command /ob add [goal]

--- src/main/java/it/forgottenworld/fwobbiettivi/command/GoalsCommandExecutor.java
+++ src/main/java/it/forgottenworld/fwobbiettivi/command/GoalsCommandExecutor.java
@@ -1,14 +1,22 @@
 package it.forgottenworld.fwobbiettivi.command;
 
+import com.palmergames.bukkit.towny.TownyUniverse;
+import com.palmergames.bukkit.towny.exceptions.NotRegisteredException;
+import com.palmergames.bukkit.towny.object.Town;
+import com.palmergames.bukkit.towny.object.WorldCoord;
 import it.forgottenworld.fwobbiettivi.FWObbiettivi;
 import it.forgottenworld.fwobbiettivi.gui.GoalsGUI;
+import it.forgottenworld.fwobbiettivi.objects.Goal;
+import it.forgottenworld.fwobbiettivi.objects.TownGoals;
 import it.forgottenworld.fwobbiettivi.utility.*;
+import org.bukkit.Location;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabExecutor;
 import org.bukkit.entity.Player;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 public class GoalsCommandExecutor implements TabExecutor {
@@ -23,52 +31,94 @@ public class GoalsCommandExecutor implements TabExecutor {
             case 1:
                 switch (args[0].toLowerCase()){
                     case CommandTypes.ADD_COMMAND:
-                        break;
-
-                    case CommandTypes.CREATE_COMMAND:
-                        break;
-
-                    case CommandTypes.DELETE_COMMAND:
-                        break;
-
-                    case CommandTypes.EDIT_COMMAND:
-                        break;
-
-                    case CommandTypes.GUI_COMMAND:
-                        // Deve essere un Player
+                        // Adding a Goal to a existing Town
+                        // Check if the sender is a player
                         if (!(sender instanceof Player)){
                             sender.sendMessage(Messages.NO_CONSOLE);
                             return true;
                         }
 
-                        Player player = (Player) sender;
-                        // Ha i permessi?
-                        if(!player.hasPermission(Permissions.PERM_GUI)){
-                            player.sendMessage(ChatFormatter.formatErrorMessage(Messages.NO_PERM));
+                        Player playerAdd = (Player) sender;
+
+                        // Save location
+                        Location loc = playerAdd.getLocation();
+
+                        // Check if the location is in a Town
+                        Town town = null;
+                        try {
+                            // Try to get the town where the player is Standing
+                            town = WorldCoord.parseWorldCoord(playerAdd.getLocation()).getTownBlock().getTown();
+                        } catch (NotRegisteredException e) {
+                            // Not in a town
+                            e.printStackTrace();
+                            playerAdd.sendMessage(ChatFormatter.formatErrorMessage(Messages.NO_TOWN_LOC));
+                        }
+
+                        // Check if the Goal exist
+                        boolean exist = false;
+                        for (Iterator<Goal> it = FWObbiettivi.instance.obbiettivi.iterator(); it.hasNext(); ) {
+                            Goal goal = it.next();
+                            if (goal.getName() == args[1]){
+                                exist = true;
+                                // Adding Goal to that Town
+                                FWObbiettivi.instance.obbiettiviInTown.add(new TownGoals(town, goal, loc));
+                            } else {
+                                playerAdd.sendMessage(ChatFormatter.formatErrorMessage(Messages.NO_GOAL) + ChatFormatter.formatWarningMessage(args[1]));
+                                return true;
+                            }
+                        }
+                        break;
+
+                    case CommandTypes.CREATE_COMMAND:
+                        // Create a new Goal
+                        break;
+
+                    case CommandTypes.DELETE_COMMAND:
+                        // Delete a Goal
+                        break;
+
+                    case CommandTypes.EDIT_COMMAND:
+                        // Edit a Goal
+                        break;
+
+                    case CommandTypes.GUI_COMMAND:
+                        // Check if the sender is a player
+                        if (!(sender instanceof Player)){
+                            sender.sendMessage(Messages.NO_CONSOLE);
+                            return true;
+                        }
+
+                        Player playerGui = (Player) sender;
+                        // Do you have the permissions?
+                        if(!playerGui.hasPermission(Permissions.PERM_GUI)){
+                            playerGui.sendMessage(ChatFormatter.formatErrorMessage(Messages.NO_PERM));
                             return true;
                         }
 
 //                        FWObbiettivi.instance.gui.setSteps(new ArrayList<Integer>());
 //                        FWObbiettivi.instance.gui.setPlayer(player);
 //                        FWObbiettivi.instance.gui.openGUI(GUIUtil.GOALS_STEP);
-                        FWObbiettivi.instance.map.put(player, new GoalsGUI());
-                        FWObbiettivi.instance.map.get(player).setPlayer(player);
-                        FWObbiettivi.instance.map.get(player).openGUI(GUIUtil.GOALS_STEP);
+                        FWObbiettivi.instance.map.put(playerGui, new GoalsGUI());
+                        FWObbiettivi.instance.map.get(playerGui).setPlayer(playerGui);
+                        FWObbiettivi.instance.map.get(playerGui).openGUI(GUIUtil.GOALS_STEP);
                         return true;
 
                     case CommandTypes.HELP_COMMAND:
+                        // Send list of available commands
                         sender.sendMessage(ChatFormatter.helpMessage());
                         return true;
 
                     case CommandTypes.MOVE_COMMAND:
+                        // Move Goal
                         break;
 
                     case CommandTypes.PAY_COMMAND:
-                        // To do
+                        // Pay all Goals
                         return true;
 
                     case CommandTypes.RELOAD_COMMAND:
-                        // Ha i permessi?
+                        // Reload the server config.yml
+                        // Do you have the permissions?
                         if(!sender.hasPermission(Permissions.PERM_RELOAD)){
                             sender.sendMessage(ChatFormatter.formatErrorMessage(Messages.NO_PERM));
                             return true;
@@ -87,9 +137,11 @@ public class GoalsCommandExecutor implements TabExecutor {
                         return true;
 
                     case CommandTypes.REMOVE_COMMAND:
+                        // Remove a Goal from a Town
                         break;
 
                     case CommandTypes.TP_COMMAND:
+                        // Teleport to a Goal
                         break;
                 }
                 break;
--- src/main/java/it/forgottenworld/fwobbiettivi/utility/Messages.java
+++ src/main/java/it/forgottenworld/fwobbiettivi/utility/Messages.java
@@ -2,8 +2,9 @@ package it.forgottenworld.fwobbiettivi.utility;
 
 public class Messages {
 
-    public static final String NO_CONSOLE = "Questo comando puo' essere eseguito solo da un player!";
-    public static final String NO_PERM = "Non hai abbastanza permessi.";
-    public static final String CONFIG_RELOAD = "Ricaricato file di configurazione.";
-
+    public static final String NO_CONSOLE = ConfigUtil.getConfigStringLang("no_console");
+    public static final String NO_PERM = ConfigUtil.getConfigStringLang("no_perm");
+    public static final String CONFIG_RELOAD = ConfigUtil.getConfigStringLang("config_reload");
+    public static final String NO_TOWN_LOC = ConfigUtil.getConfigStringLang("no_town_loc");
+    public static final String NO_GOAL = ConfigUtil.getConfigStringLang("no_goal");
 }
