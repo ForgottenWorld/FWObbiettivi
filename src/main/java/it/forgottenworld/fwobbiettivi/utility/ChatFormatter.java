ce5a4d0c700a6564ff2d4847390ac5e6af6a91b5
Markus27 <marco.demicheli.27@gmail.com>
2020-11-02 19:41:53 +0100

    Edited help command list with command /ob list and edited the description of command /ob pay

--- src/main/java/it/forgottenworld/fwobbiettivi/utility/ChatFormatter.java
+++ src/main/java/it/forgottenworld/fwobbiettivi/utility/ChatFormatter.java
@@ -70,9 +70,10 @@ public class ChatFormatter {
                 "\n" + ChatColor.GOLD + "> " + ChatColor.DARK_AQUA + "/ob " + ChatColor.AQUA + "enable " + "<Obbiettivo> <Town>" + ChatColor.GRAY + ": Abilita un obbiettivo." +
                 "\n" + ChatColor.GOLD + "> " + ChatColor.DARK_AQUA + "/ob " + ChatColor.AQUA + "gui " + ChatColor.GRAY + ": Apre la GUI." +
                 "\n" + ChatColor.GOLD + "> " + ChatColor.DARK_AQUA + "/ob " + ChatColor.AQUA + "help " + ChatColor.GRAY + ": Mostra questa lista." +
+                "\n" + ChatColor.GOLD + "> " + ChatColor.DARK_AQUA + "/ob " + ChatColor.AQUA + "list " + ChatColor.GRAY + ": Mostra una lista di tutti gli obbiettivi presenti." +
                 "\n" + ChatColor.GOLD + "> " + ChatColor.DARK_AQUA + "/ob " + ChatColor.AQUA + "move " + "<Obbiettivo> <Town>" + ChatColor.GRAY + ": Sposta un obbiettivo di una Town sulla tua posizione." +
                 "\n" + ChatColor.GOLD + "> " + ChatColor.DARK_AQUA + "/ob " + ChatColor.AQUA + "new " + "<Obbiettivo> " + ChatColor.GRAY + ": Crea un nuovo obbiettivo." +
-                "\n" + ChatColor.GOLD + "> " + ChatColor.DARK_AQUA + "/ob " + ChatColor.AQUA + "pay " + ChatColor.GRAY + ": Paga tutti gli obbiettivi presenti." +
+                "\n" + ChatColor.GOLD + "> " + ChatColor.DARK_AQUA + "/ob " + ChatColor.AQUA + "pay " + ChatColor.GRAY + ": Paga tutti gli obbiettivi abilitati." +
                 "\n" + ChatColor.GOLD + "> " + ChatColor.DARK_AQUA + "/ob " + ChatColor.AQUA + "reload " + ChatColor.GRAY + ": Ricarica il config.yml." +
                 "\n" + ChatColor.GOLD + "> " + ChatColor.DARK_AQUA + "/ob " + ChatColor.AQUA + "remove " + "<Obbiettivo> <Town>" + ChatColor.GRAY + ": Rimuove un obbiettivi a una Town." +
                 "\n" + ChatColor.GOLD + "> " + ChatColor.DARK_AQUA + "/ob " + ChatColor.AQUA + "tp " + "<Obbiettivo> <Town>" + ChatColor.GRAY + ": Teletrasporta il player ad un determinato obbiettivo in una Town."
