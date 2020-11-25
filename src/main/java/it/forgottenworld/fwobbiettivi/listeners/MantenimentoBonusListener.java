30b5884b0a5dfbfc8dad89bd4432c605d899fbf5
Markus27 <marco.demicheli.27@gmail.com>
2020-11-25 23:28:45 +0100

    Fix upkeep v2

--- src/main/java/it/forgottenworld/fwobbiettivi/listeners/MantenimentoBonusListener.java
+++ src/main/java/it/forgottenworld/fwobbiettivi/listeners/MantenimentoBonusListener.java
@@ -8,17 +8,21 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 public class MantenimentoBonusListener implements Listener {
 
     private static HashMap<String, Double> plotMantenimento = new HashMap<String, Double>();
 
+    private static List<String> exemprion = new ArrayList<>();
+
     @EventHandler(priority = EventPriority.HIGH)
     public void onTownUpkeepCalculated(TownUpkeepCalculationEvent e) {
         updateDatasFromConfig();
         for (TownGoal tg: TownGoals.getObbiettiviInTown()){
-            if (e.getTown().equals(tg.getTown()))
+            if (e.getTown().equals(tg.getTown()) && !exemprion.contains(e.getTown().getName()))
                 e.setUpkeep(e.getUpkeep() - ((e.getUpkeep() / 100) * plotMantenimento.get(tg.getGoal().getName())));
         }
     }
@@ -27,6 +31,13 @@ public class MantenimentoBonusListener implements Listener {
         plotMantenimento.clear();
         for (String s : FWObbiettivi.getInstance().getConfig().getConfigurationSection("mantenimentoBonus.plotNames").getKeys(false))
             plotMantenimento.put(s, FWObbiettivi.getInstance().getConfig().getDouble("mantenimentoBonus.plotNames." + s));
+
+        exemprion.clear();
+        List<String> list = FWObbiettivi.getInstance().getConfig().getStringList("exemprionBonus");
+        if (!list.isEmpty())
+            exemprion.addAll(list);
+
+        System.out.println(exemprion.get(0));
     }
 
 }
